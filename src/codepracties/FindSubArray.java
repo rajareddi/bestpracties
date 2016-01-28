ae82f20bc313a00e3a0a87acc46d671c3b98e222
rkarremolla <rkarremolla@ciphercloud.com>
2016-01-28 10:57:30 +0530

    Find sum2

---
+++ src/codepracties/FindSubArray.java
@@ -0,0 +1,50 @@
+package codepracties;
+
+//Find the subarray with least average
+public class FindSubArray {
+
+	public static void main(String[] args) {
+		{
+			int arr[] = { 3, 7, 90, 20, 10, 50, 40 };
+			int k = 3; // Subarray size
+			int n = arr.length;
+			findMinAvgSubarray(arr, n, k);
+			return;
+		}
+	}
+
+	private static void findMinAvgSubarray(int[] arr, int n, int k) {
+		// Prints beginning and ending indexes of subarray
+		// of size k with minimum average
+		// k must be smaller than or equal to n
+		if (n < k)
+			return;
+
+		// Initialize beginning index of result
+		int res_index = 0;
+
+		// Compute sum of first subarray of size k
+		int curr_sum = 0;
+		for (int i = 0; i < k; i++)
+			curr_sum += arr[i];
+
+		// Initialize minimum sum as current sum
+		int min_sum = curr_sum;
+
+		// Traverse from (k+1)'th element to n'th element
+		for (int i = k; i < n; i++) {
+			// Add current item and remove first item of
+			// previous subarray
+			curr_sum += arr[i] - arr[i - k];
+
+			// Update result if needed
+			if (curr_sum < min_sum) {
+				min_sum = curr_sum;
+				res_index = (i - k + 1);
+			}
+		}
+
+		System.out.println("Subarray between " + res_index + "<< "
+				+ (res_index + k - 1) + "] has minimum average");
+	}
+}
